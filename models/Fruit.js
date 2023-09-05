2caa44c5b2e17e9b7003e5bfd625ddc1f22e3424
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2023-09-05 14:21:36 +0530

    Update Fruit.js

--- models/Fruit.js
+++ models/Fruit.js
@@ -1,5 +1,42 @@
+class Fruit {
+    constructor(id, name, color) {
+      this.id = id;
+      this.name = name;
+      this.color = color;
+    }
+  }
+  
+  module.exports = Fruit;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // models/Fruit.js
-1
+
 class Fruit {
     constructor(id, name, color) {
       this.id = id;
