aaa14260397a23b0f3ce8a509e95a9821fcbe51e
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2023-09-05 14:23:05 +0530

    Update Fruit.js

--- models/Fruit.js
+++ models/Fruit.js
@@ -5,7 +5,130 @@ class Fruit {
     constructor(id, name, color) {
       thisclass Fruit {
     constructclass Fruit {
+    constructor(id,class Fruit {
     constructor(id, name, color) {
+      this.id = id;
+      thisclass Fruit {
+    constructor(id, name, color) {
+      this.id = class Fruit {
+    constructor(id, name, color) {
+      this.id = id;
+      this.name class Fruit {
+    constructor(id, name, color) {
+      this.id = id;
+      thisclass Fruit {
+    constructor(id, name, color) {
+      this.id = class Fruit {
+    constructor(id, name, color) {
+      this.id = id;
+      this.name = name;
+      this.class Fruit {
+    constructor(id, name, color) {
+      this.id = id;
+      this.name = name;
+      this.color = color;
+    }
+  }
+  
+  module.exports = Fruit;color = color;
+    }
+  }
+  
+  module.exports = Fruit;id;
+      this.name = name;
+      this.color = color;
+    }
+  }
+  
+  module.exports = Fruit;.name = name;
+      this.color = color;
+    }
+  }
+  
+  module.exports = Fruit;class Fruit {
+    constructor(id, name, color) {
+      this.id = id;
+      this.name = name;
+      this.color = color;
+    }
+  }
+  
+  module.exports = Fruit;
+= name;
+      this.class Fruit {
+    constructor(id, name, color) {
+      this.id = id;class Fruit {
+    constructor(id, name, color) {
+      this.id = id;
+      thisclass Fruit {
+    constructor(id, name, color) {
+      this.id = class Fruit {
+    constructor(id, name, color) {
+      this.id = id;
+      this.name = name;
+      this.class Fruit {
+    constructor(id, name, color) {
+      this.id = id;
+      this.name = name;
+      this.color = color;
+    }
+  }
+  
+  module.exports = Fruit;color = color;
+    }
+  }
+  
+  module.exports = Fruit;id;
+      this.name = name;
+      this.color = color;
+    }
+  }
+  
+  module.exports = Fruit;.name = name;
+      this.color = color;
+    }
+  }
+  
+  module.exports = Fruit;class Fruit {
+    constructor(id, name, color) {
+      this.id = id;
+      this.name = name;
+      this.color = color;
+    }
+  }
+  
+  module.exports = Fruit;
+
+      this.name = name;
+      this.color = color;
+    }
+  }
+  
+  module.exports = Fruit;color = color;
+    }
+  }
+  
+  module.exports = Fruit;id;
+      this.name = name;
+      this.color = color;
+    }
+  }
+  
+  module.exports = Fruit;.name = name;
+      this.color = color;
+    }
+  }
+  
+  module.exports = Fruit;class Fruit {
+    constructor(id, name, color) {
+      this.id = id;
+      this.name = name;
+      this.color = color;
+    }
+  }
+  
+  module.exports = Fruit;
+ name, color) {
       this.id = id;
       thisclass Fruit {
     constructor(id, name, color) {
