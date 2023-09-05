3a2f7e4475a3c95abfb3569ec546735ac569c1e5
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2023-09-05 14:24:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 // app.js
 // app.js
 // app.js
+// app.js
+
+const express = require('express');
+const app = express();
+
+const fruitRoutes = require('./routes/fruitRoutes');
+
+// Mount the fruitRoutes under the '/fruits' path
+
+app.use('/', fruitRoutes);
+
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
 
 const express = require('express');
 const app = express();
